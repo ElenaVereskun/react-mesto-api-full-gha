e0c2e27dff31a99480485ea02d3f587dbabe6347
Elena Vereskun <lena.vereskun@yandex.ru>
2023-08-09 15:41:19 +0300

    testTest

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -21,8 +21,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   function handleDeleteClick() {
     onCardDelete(card);
   }
-  // Определяем, являемся ли мы владельцем текущей карточки  
-  console.log(card);
+  // Определяем, являемся ли мы владельцем текущей карточки
   const isOwn = card.owner === currentUser._id;
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
   const isLiked = card.likes.some(i => i === currentUser._id);
